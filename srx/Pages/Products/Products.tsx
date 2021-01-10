001f2eef57e3e51d67c9dda51e28929194b6f9d2
mahesh K S <mahesh25242@gmail.com>
2021-01-10 06:20:48 +0530

    x

--- srx/Pages/Products/Products.tsx
+++ srx/Pages/Products/Products.tsx
@@ -1,13 +1,45 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import * as React from 'react';
 import { ProductService } from '../../services';
+import { Table } from 'antd';
 
 const Products = () => {
     const productService = new ProductService();
-    productService.products().subscribe(res=>{
-        console.log(res)
-    })
-    return (<div>Products Page</div>);
+    const [products, SetProducts] = React.useState(null);
+
+    React.useEffect(() => {
+        productService.products().subscribe(res=>{
+            SetProducts(res.response.data);
+        })
+
+    }, []);
+
+   
+
+    const columns = [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Description',
+          dataIndex: 'description',
+          key: 'description',
+        },
+        {
+          title: 'Created At',
+          dataIndex: 'created_at',
+          key: 'created_at',
+        },
+      ];
+      
+      
+
+    console.log(products)
+    return (<>
+       products &&   <Table dataSource={products} columns={columns} />
+    </>);
 };
 
 export default Products;
