2dfd0c3301654e19cbf11f8c4908d7f46eebefa9
mahesh K S <mahesh25242@gmail.com>
2021-01-13 13:00:10 +0530

    c

--- srx/Pages/Billing/Billing.tsx
+++ srx/Pages/Billing/Billing.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import * as React from 'react';
 import { ProductService } from '../../services';
-import { Button, Form, Input, PageHeader, Table } from 'antd';
+import { Button, Form, Input, InputNumber, PageHeader, Select, Table } from 'antd';
 import { Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 import { AutoComplete } from 'antd';
@@ -11,11 +11,16 @@ const { Option } = AutoComplete;
 
 const Billing:React.FC = () => {
     const productService = new ProductService();
-    const [products, SetProducts] = React.useState<any>(null);
+    const [products, setProducts] = React.useState<any>(null);
+    const [options, setOptions] = React.useState<any>(null);
+    const [product, setProduct] = React.useState<any>(null);
+    
+    const [form] = Form.useForm();
 
     React.useEffect(() => {
         const prodSubscr = productService.products().subscribe(res=>{
-          SetProducts(res.response.data);
+          setProducts(res.response.data);      
+          setOptions(options);                      
         });
 
         return () => {
@@ -24,6 +29,7 @@ const Billing:React.FC = () => {
     }, []);
 
    
+    
 
     const columns = [
         {
@@ -71,14 +77,45 @@ const Billing:React.FC = () => {
         let res: string[] = [];
         if (!value) {
           res = [];
-        }         
-        SetProducts(products);
+        }      
+        
+        if(products)
+          res = products.filter( (option:any) => option.name.toUpperCase().indexOf(value.toUpperCase()) !== -1);
+
+              
+        
+          setOptions(res);
       };
       
       const handleSelect = (value: any, option:any) => {
-        console.log(value, option)
+        const res = products.filter( (product:any) => product.id == option.key );
+        
+
+        setProduct(res[0]); 
+        
+        form.setFieldsValue({
+          varient: res[0] && res[0].shop_product_primary_variant.id
+        });
+    
       }
 
+      function onChange(value: any) {
+        console.log(`selected ${value}`);
+      }
+      
+      function onBlur() {
+        console.log('blur');
+      }
+      
+      function onFocus() {
+        console.log('focus');
+      }
+      
+      function onSearch(val: any) {
+        console.log('search:', val);
+      }
+
+      console.log(product)
     
     return (<>
       <PageHeader
@@ -89,32 +126,82 @@ const Billing:React.FC = () => {
           <Button key="1"><Link to="/products">List Product</Link></Button>,          
         ]}
       />
-      <Form        
+      <Form  
+        form={form}       
         name="basic"
-        initialValues={{ remember: true }}
+        initialValues={{ quantity: 1 }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
       >
       <Row>
-        <Col span={24}>
+        <Col span={ (product && product.shop_product_variant)  ? 12 : 24 }>
             <Form.Item
             label="Product"
             name="product"
             rules={[{ required: true, message: 'Please enter a product!' }]}
           >
-            <AutoComplete style={{ width: 200 }} 
+            <AutoComplete  
+            allowClear
             onSearch={handleSearch} 
             placeholder="Enter the product here" 
             onSelect={handleSelect}>
-            {products && products.map((product: any) => (
-              <Option key={product.id} value={product.name}>
-                {product.name}
+            {options && options.map((option: any) => (
+              <Option key={option.id} value={option.name}>
+                {option.name}
               </Option>
             ))}
           </AutoComplete>
           </Form.Item>
         </Col>
+        {
+          product && product.shop_product_variant && product.shop_product_variant.length > 1 && 
+          <Col span={12}>
+            <Form.Item
+            label="Varient"
+            name="varient"
+            rules={[{ required: true, message: 'Please choose a varient!' }]}
+            >
+              <Select
+                showSearch  
+                allowClear          
+                placeholder="Select a Varient"
+                optionFilterProp="children"
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                onSearch={onSearch}
+                filterOption={(input, option) =>
+                  option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                }
+              >{
+                product.shop_product_variant.map((spv: any) => <Option key={spv.id} value={spv.id}>{spv.name}</Option>)
+              }            
+              </Select>
+            </Form.Item>
+        </Col>
+        }
+        
       </Row>
+      <Row>
+        <Col span="4">
+          <Form.Item label="Quantity"
+          name="quantity" 
+          rules={[{ required: true, message: 'Please enter quantity' }]}>
+            <InputNumber />
+          </Form.Item>
+
+          <Form.Item name="message" label="Message">
+            <Input.TextArea />
+          </Form.Item>
+          
+        </Col>
+      </Row>
+      <Form.Item>
+          <Button type="primary" htmlType="submit" loading={false}>
+              Save & Print
+          </Button>
+      </Form.Item>
+
       </Form>
        {
        products &&   <Table dataSource={products} 
