8dfb38408aec8aaf4255818d9041b6df200e7ba8
mahesh K S <mahesh25242@gmail.com>
2021-02-04 14:51:31 +0530

    v

--- srx/Pages/Orders/ListOrders.tsx
+++ srx/Pages/Orders/ListOrders.tsx
@@ -11,6 +11,9 @@ import { Link, useRouteMatch } from 'react-router-dom';
 import { MyBreadcrumb } from '../../SharedComponents';
 import { Subscription } from 'rxjs';
 
+import { remote } from 'electron';
+const { Menu, MenuItem } = remote;
+
 const ListOrders = () => {    
     const orderService = new OrderService();
     const [orders, SetOrders] = React.useState(null);
@@ -22,6 +25,18 @@ const ListOrders = () => {
     const { path, url } = useRouteMatch();    
     let  prodSubscr:Subscription;
     React.useEffect(() => {
+
+      // const menu = new Menu()
+      // menu.append(new MenuItem({ label: 'MenuItem1', click(evt) { console.log('item 1 clicked', evt) } }))
+      // menu.append(new MenuItem({ type: 'separator' }))
+      // menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }))
+
+      // window.addEventListener('contextmenu', (e) => {
+      //   e.preventDefault()
+      //   menu.popup({ window: remote.getCurrentWindow() })
+      // }, false)
+
+
         prodSubscr = orderService.orders().subscribe(res=>{
           SetOrders(res.response.data);
           SetTotalPage(res.response.total)
@@ -57,6 +72,12 @@ const ListOrders = () => {
           dataIndex: 'delivery',
           key: 'delivery',
           render: (text: string, record: any, index: number) => <>{record.shop_delivery.name}</>,
+        },
+        {
+          title: 'Options',
+          dataIndex: 'options',
+          key: 'options',
+          render: (text: string, record: any, index: number) => <><a>To Billing</a></>,
         },
       ];
       
